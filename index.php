cdc05488bc98876ef27412332bcad1095773f8d8
Mariam-26 <82433410+Mariam-26@users.noreply.github.com>
2022-04-25 04:54:32 +0200

    mariPhp

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+// On inclut le header
+ include_once "includes/header.php";
+
+// on inclut la navbar
+ include_once "includes/navbar.php";
+  /* diviser le code*/ 
+
+ // On inclut le footer
+  include "includes/footer.php";
+  
+echo ("Bonjour !");
